ce30f7276fc00c5112777b5a4a4e690116461c7a
William Ehlhardt <whale@dropbox.com>
2020-07-11 01:39:15 +0000

    Intentionally reintroduce bug to enable testing

--- src/main/java/org/kairosdb/datastore/cassandra/BatchHandler.java
+++ src/main/java/org/kairosdb/datastore/cassandra/BatchHandler.java
@@ -125,6 +125,7 @@ public class BatchHandler extends RetryCallable
 
 			//Write out the row key if it is not cached
 			DataPointsRowKey cachedKey = m_rowKeyCache.get(rowKey);
+			m_rowKeyCache.put(rowKey);
 			if (cachedKey == null)
 			{
 				batch.addRowKey(metricName, rowKey, rowKeyTtl);
@@ -136,6 +137,7 @@ public class BatchHandler extends RetryCallable
 
 			//Write metric name if not in cache
 			String cachedName = m_metricNameCache.get(metricName);
+			m_metricNameCache.put(metricName);
 			if (cachedName == null)
 			{
 				if (metricName.length() == 0)
