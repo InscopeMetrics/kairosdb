cfb9668fff3fc6e6cbc31707d5442faed4845b36
Ville Koskela <ville@koskelafamily.com>
2020-11-05 14:42:31 -0800

    Addressed feedback.

--- src/test/java/org/kairosdb/core/health/DatastoreQueryHealthCheckTest.java
+++ src/test/java/org/kairosdb/core/health/DatastoreQueryHealthCheckTest.java
@@ -1,5 +1,6 @@
 package org.kairosdb.core.health;
 
+import com.google.common.collect.Sets;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,28 +8,32 @@ import org.kairosdb.core.datastore.DatastoreQuery;
 import org.kairosdb.core.datastore.KairosDatastore;
 import org.kairosdb.core.datastore.QueryMetric;
 import org.kairosdb.core.exception.DatastoreException;
+import org.mockito.ArgumentCaptor;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static com.codahale.metrics.health.HealthCheck.Result;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class DatastoreQueryHealthCheckTest {
+
     private KairosDatastore datastore;
     private DatastoreQuery query;
-    private DatastoreQueryHealthCheck healthCheck;
 
     @Before
     public void setup() throws DatastoreException {
         datastore = mock(KairosDatastore.class);
         query = mock(DatastoreQuery.class);
         when(datastore.createQuery(any(QueryMetric.class))).thenReturn(query);
-
-        healthCheck = new DatastoreQueryHealthCheck(datastore, "foo/bar", null);
     }
 
     @Test(expected = NullPointerException.class)
@@ -36,15 +41,60 @@ public class DatastoreQueryHealthCheckTest {
         new DatastoreQueryHealthCheck(null, "foo/bar", null);
     }
 
-    @Test
-    public void testCheckHealthy() throws Exception {
-        final Result result = healthCheck.check();
+    @Test(expected = RuntimeException.class)
+    public void testConstructorTagsInvalid() {
+        new DatastoreQueryHealthCheck(null, "foo/bar", "foo,1,bar");
+    }
 
+    @Test
+    public void testCheckHealthyNoTagsNull() throws Exception {
+        final DatastoreQueryHealthCheck healthCheck = new DatastoreQueryHealthCheck(
+                datastore,
+                "foo/bar",
+                null);
+        final Result result = healthCheck.check();
+        final ArgumentCaptor<QueryMetric> captor = ArgumentCaptor.forClass(QueryMetric.class);
+        verify(datastore).createQuery(captor.capture());
+        assertTrue(captor.getValue().getTags().isEmpty());
+        assertTrue(result.isHealthy());
+    }
+
+    @Test
+    public void testCheckHealthyNoTagsEmpty() throws Exception {
+        final DatastoreQueryHealthCheck healthCheck = new DatastoreQueryHealthCheck(
+                datastore,
+                "foo/bar",
+                "");
+        final Result result = healthCheck.check();
+        final ArgumentCaptor<QueryMetric> captor = ArgumentCaptor.forClass(QueryMetric.class);
+        verify(datastore).createQuery(captor.capture());
+        assertTrue(captor.getValue().getTags().isEmpty());
+        assertTrue(result.isHealthy());
+    }
+
+    @Test
+    public void testCheckHealthyWithTags() throws Exception {
+        final DatastoreQueryHealthCheck healthCheck = new DatastoreQueryHealthCheck(
+                datastore,
+                "foo/bar",
+                "abc,123,def,456,def,789");
+        final Result result = healthCheck.check();
+        final ArgumentCaptor<QueryMetric> captor = ArgumentCaptor.forClass(QueryMetric.class);
+        when(datastore.createQuery(any(QueryMetric.class))).thenReturn(query);
+        verify(datastore).createQuery(captor.capture());
+        assertEquals(3, captor.getValue().getTags().size());
+        assertEquals(Collections.singleton("123"), captor.getValue().getTags().get("abc"));
+        assertEquals(Sets.newHashSet(Arrays.asList("456", "789")), captor.getValue().getTags().get("def"));
         assertTrue(result.isHealthy());
     }
 
     @Test
     public void testCheckUnHealthy() throws Exception {
+        final DatastoreQueryHealthCheck healthCheck = new DatastoreQueryHealthCheck(
+                datastore,
+                "foo/bar",
+                null);
+
         final Exception exception = new DatastoreException("Error message");
         when(query.execute()).thenThrow(exception);
 
@@ -57,6 +107,10 @@ public class DatastoreQueryHealthCheckTest {
 
     @Test
     public void testGetName() {
+        final DatastoreQueryHealthCheck healthCheck = new DatastoreQueryHealthCheck(
+                datastore,
+                "foo/bar",
+                null);
         assertThat(healthCheck.getName(), equalTo(DatastoreQueryHealthCheck.NAME));
     }
 }
